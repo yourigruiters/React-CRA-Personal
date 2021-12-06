cb61b39be8717ae153f1568f6e335cd27c6d7b66
Youri Gruiters <i-youri-i@hotmail.com>
2021-12-06 13:33:30 +0100

    Update webpack and tsconfig files

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ const __dirname = path.resolve();
 
 export default {
   entry: './src/index.tsx',
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['*', '.ts', '.tsx', '.js', '.json'],
@@ -23,20 +23,22 @@ export default {
         exclude: /node_modules/,
         loader: 'ts-loader',
       },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       {
         test: /\.(sa|sc|c)ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(mp4|svg|png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
-  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/assets/index.html',
